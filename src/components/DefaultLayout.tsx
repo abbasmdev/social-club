0bcf5f9bd43a8d40047ad722dd1da9af95f94588
Abbas Moharami <abbasmdev@gmail.com>
2023-02-03 22:54:25 +0330

    feat: Add Header menu
    - Add headlessui package
    - Add header menu with logout button
    - Update auth middleware

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -1,8 +1,11 @@
+import { Menu } from "@headlessui/react";
+import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid";
+import clx from "classnames";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { type FC } from "react";
-import clx from "classnames";
+
 const DefaultLayout: FC<{
   children: React.ReactNode;
   headerExtra?: React.ReactNode;
@@ -11,7 +14,6 @@ const DefaultLayout: FC<{
   const { data: session } = useSession();
   const userName = session?.user?.name;
   const userImage = session?.user?.image;
-  console.log("a>", userImage);
 
   return (
     <div className="flex h-screen flex-col gap-6">
@@ -20,19 +22,36 @@ const DefaultLayout: FC<{
           <Link href={"/"}>
             👋 <span className="text-lg font-semibold">SocialClub</span>
           </Link>
-          <div className="flex items-center gap-3" onClick={() => signOut()}>
-            <span className="font-semibold">{userName}</span>
-            {userImage && (
-              <div className="relative h-10 w-10 ">
-                <Image
-                  src={userImage}
-                  fill
-                  className="rounded-full"
-                  alt="profile picture"
-                />
-              </div>
-            )}
-          </div>
+          <Menu as="div" className={"relative inline-block"}>
+            <Menu.Button className="flex items-center gap-3">
+              <span className="font-semibold">{userName}</span>
+              {userImage && (
+                <div className="relative h-10 w-10 ">
+                  <Image
+                    src={userImage}
+                    fill
+                    className="rounded-full"
+                    alt="profile picture"
+                  />
+                </div>
+              )}
+            </Menu.Button>
+            <Menu.Items
+              className={
+                "absolute right-0 z-20   w-44  rounded-sm bg-white p-2 shadow-sm"
+              }
+            >
+              <Menu.Item>
+                <button
+                  onClick={() => signOut()}
+                  className="flex w-full items-center gap-2 "
+                >
+                  <ArrowLeftOnRectangleIcon className="h-5 w-5" />
+                  Logout
+                </button>
+              </Menu.Item>
+            </Menu.Items>
+          </Menu>
         </div>
         {headerExtra ? <div>{headerExtra}</div> : null}
       </header>
